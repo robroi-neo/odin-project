5bf9bc419fbae5fc5514fd804e6dc5c39cb59122
robroi-neo <robroineo2@gmail.com>
2026-05-31 23:03:51 +0800

    track

---
+++ my-first-react-app/src/Greeting.jsx
@@ -0,0 +1,13 @@
+function Greeting() {
+    return (
+    <>
+        <h1>List of Foods!</h1>
+        <ul>
+            <li>Coffee</li>
+            <li>Pasta</li>
+        </ul>
+    </>
+    )
+}
+
+export default Greeting;
